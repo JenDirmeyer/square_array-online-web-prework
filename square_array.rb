1ccee3148d68a0f29f424b5385167e295aa3d7d0
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-24 21:21:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared_array = [ ]
   array.each do |element|
-    squared-array.push("element ** 2") 
+    squared-array.push(element ** 2) 
   end 
 end 
    squared_array
